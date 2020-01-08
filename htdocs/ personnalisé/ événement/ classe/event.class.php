eac8c3062f87867f4c187abf57cc3a717064bc30
Vincent <47563262+tlse-vins@users.noreply.github.com>
2020-01-08 16:38:51 +0100

    Update and rename htdocs/custom/event/class/event.class.php to  htdocs/ personnalisé/ événement/ classe/event.class.php

--- htdocs/ personnalisé/ événement/ classe/event.class.php
+++ htdocs/ personnalisé/ événement/ classe/event.class.php
@@ -192,9 +192,8 @@ class Event extends CommonObject
 	            /*
 	             *  Create event_day on the event
 	             */
-	            if($conf->global->DISABLE_CREATE_1ST_BAY_BY_DEFAULT=="0") {
-		            include_once(DOL_DOCUMENT_ROOT . "/core/lib/date.lib.php");
-		            if($this->date_end > 0)
+	            if($conf->global->DISABLE_CREATE_1ST_DAY_BY_DEFAULT=="0") {
+			    include_once(DOL_DOCUMENT_ROOT . "/core/lib/date.lib.php");
 		            {
 		            	// Nb days between date_start & date_end
 		            	$nb_days = num_between_day($this->date_start,$this->date_end,1);
