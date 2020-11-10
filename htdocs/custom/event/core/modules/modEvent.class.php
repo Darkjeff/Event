36e149899089093b0f62d35cff53204bba63acce
Vincent <47563262+tlse-vins@users.noreply.github.com>
2020-11-10 11:17:31 +0100

    Update modEvent.class.php

--- htdocs/custom/event/core/modules/modEvent.class.php
+++ htdocs/custom/event/core/modules/modEvent.class.php
@@ -46,7 +46,7 @@ class modEvent extends DolibarrModules
 
 		// Id for module (must be unique).
 		// Use here a free id (See in Home -> System information -> Dolibarr for list of used modules id).
-		$this->numero = 448030;
+		$this->numero = 508030;
 		// Key text used to identify module (for permissions, menus, etc...)
 		$this->rights_class = 'event';
 
